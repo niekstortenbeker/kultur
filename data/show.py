29416bc9ae17d754c734cf23f8528bff714e77ea
Niek Stortenbeker <niekns@gmail.com>
2020-04-09 11:22:05 +0200

    solved bugs

--- data/show.py
+++ data/show.py
@@ -13,15 +13,15 @@ class Show(SqlAlchemyBase):
     location = sa.Column(sa.String, index=True)
     # category = 'cinema', 'music' or 'stage'
     category = sa.Column(sa.String, index=True)
-    creation_date = sa.Column(sa.DateTime, default=arrow.utcnow)
+    creation_date = sa.Column(ArrowType, default=arrow.utcnow)
     description = sa.Column(sa.String, nullable=True)
     language_version = sa.Column(sa.String, nullable=True)
-    dubbed = sa.Column(sa.Boolean, default=False)
+    dubbed = sa.Column(sa.Boolean, nullable=True, default=False)
     url_info = sa.Column(sa.String, nullable=True)
     url_tickets = sa.Column(sa.String, nullable=True)
 
     def __repr__(self):
-        return "Show()"
+        return f"Show({self.location}, {self.title})"
 
     def __str__(self):
         return f"Show(id='{self.id}', date_time='{self.date_time}', title='{self.title}', location='{self.location}', category='{self.category}', creation_date='{self.creation_date}', language_version='{self.language_version}', dubbed='{self.dubbed}', url_info='{self.url_info}', url_tickets='{self.url_tickets}', description='{self.description}')"
